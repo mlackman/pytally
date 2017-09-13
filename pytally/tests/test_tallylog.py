bb74fddeb6d00d6870c06e79f369e0291a3b5a22
Mika Lackman <mika.lackman@upcloud.com>
2017-09-13 23:11:17 +0300

    Created Line abstraction and refactored code and fixed few bugs

--- pytally/tests/test_tallylog.py
+++ pytally/tests/test_tallylog.py
@@ -69,6 +69,16 @@ class TestTallyLog(unittest.TestCase):
 
         self.assert_tally_contains(['release 1', 'release 2'])
 
+    def test_remove_single_tag(self):
+        self.t.add('release 1')
+        self.t.add('release 2')
+        self.t.tag('release 1', 'current')
+        self.t.tag('release 2', 'other')
+
+        self.t.remove_tag('current')
+
+        self.assert_tally_contains(['release 1', 'release 2 [other]'])
+
     def test_removing_non_existing_tag(self):
         self.t.add('release 1')
         self.t.add('release 2')
@@ -82,9 +92,27 @@ class TestTallyLog(unittest.TestCase):
         self.t.add('release 2')
         self.t.tag('release 1', 'current')
 
-        self.t.move_tag('release 2', 'current')
+        self.t.move_tag('release 1', 'release 2')
 
         self.assert_tally_contains(['release 1', 'release 2 [current]'])
+
+    def test_move_tag_from_line_which_does_not_exists(self):
+        self.t.add('release 2')
+        with self.assertRaises(tallylog.NoSuchLineFound):
+            self.t.move_tag('release 1', 'release 2')
+
+    def test_move_tag_to_line_which_does_not_exists(self):
+        self.t.add('release 1')
+        self.t.tag('release 1', 'current')
+        with self.assertRaises(tallylog.NoSuchLineFound):
+            self.t.move_tag('release 1', 'release 2')
+
+    def test_move_tag_when_tag_does_not_exists(self):
+        self.t.add('release 1')
+        self.t.add('release 2')
+
+        with self.assertRaises(tallylog.TagNotFound):
+            self.t.move_tag('release 1', 'release 2')
 
     def test_move_tag_up(self):
         self.t.add('release 1')
@@ -96,6 +124,17 @@ class TestTallyLog(unittest.TestCase):
 
         self.t.move_tag_up('current')
         self.assert_tally_contains(['release 1 [current]', 'release 2', 'release 3 [current]', 'release 4'])
+
+    def test_move_tag_does_not_move_unrelated_tag(self):
+        self.t.add('release 1')
+        self.t.add('release 2')
+        self.t.add('release 3')
+        self.t.add('release 4')
+        self.t.tag('release 2', 'current')
+        self.t.tag('release 3', 'other')
+
+        self.t.move_tag_up('current')
+        self.assert_tally_contains(['release 1 [current]', 'release 2', 'release 3 [other]', 'release 4'])
 
     def test_move_tag_up_when_tag_does_not_exist(self):
         self.t.add('release 1')
