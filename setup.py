5c3503f026b5ba47cb42c84654b700b1aa2f7029
Mika Lackman <mika.lackman@upcloud.com>
2017-09-18 10:00:55 +0300

    version increase

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytally',
-      version='0.4',
+      version='0.5',
       description='Simple bookkeeping',
       url='https://github.com/mlackman/pytally',
       author='Mika Lackman',
